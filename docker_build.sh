eb24dbd057df11244726c98869c8206a7f6c04a7
eldelto <shark77@live.at>
2018-08-05 12:00:00 +0200

    Fixed docker_build.sh error.

--- docker_build.sh
+++ docker_build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir docker_images
-docker build -tag="temporary_server:latest" .
+docker build --tag="temporary_server:latest" .
 docker save -o ./docker_images/temporary_server_image.tar temporary_server
