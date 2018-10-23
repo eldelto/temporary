4f2d4751bdc1b59dbd65c4383d786238f6125c89
eldelto <shark77@live.at>
2018-10-23 21:34:10 +0200

    Fixed dependency issue.

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule TemporaryServer.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"},
+      {:plug_cowboy, "~> 1.0"},
       {:distillery, "~> 1.5", runtime: false},
       {:mix_test_watch, "~> 0.6", only: :dev, runtime: false},
       {:chunker, "~> 0.12.0"}
